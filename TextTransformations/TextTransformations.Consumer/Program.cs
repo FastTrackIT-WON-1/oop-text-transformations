9ddf5561482f66384556b9ad015bcb8db3511ab4
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-01-27 18:32:55 +0200

    Initial project setup

---
+++ TextTransformations/TextTransformations.Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextTransformations.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TextTransformations/TextTransformations.Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TextTransformations.Library
+{
+    public class Class1
+    {
+    }
+}
